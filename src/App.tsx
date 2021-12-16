02abc64bf4077a7b0cd2217f457abdef7518b92f
kev <thehailang@gmail.com>
2021-12-17 03:00:37 +0800

    :Thu Dec 16 19:00:37 UTC 2021

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
+import Uppy from '@uppy/core';
+import { Dashboard, useUppy } from '@uppy/react';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const uppy = useUppy(() => {
+    return new Uppy();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Dashboard uppy={uppy}/>
   );
 }
 
