be08cca5ffbee5313d02b594bb3c47d334c89def
kev <thehailang@gmail.com>
2021-12-17 03:17:56 +0800

    :Thu Dec 16 19:17:56 UTC 2021

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import Uppy from '@uppy/core';
+import '@uppy/core/dist/style.css';
+import '@uppy/dashboard/dist/style.css';
 import { Dashboard, useUppy } from '@uppy/react';
 import React from 'react';
 import './App.css';
